0482f659069aadd63e9d93f77b4f62dc5f588401
Chris Sherwood Win10 desktop <csherwood@usgs.gov>
2020-02-09 18:15:07 -0500

    deal with all-nan profiles

--- CoreBx_funcs.py
+++ CoreBx_funcs.py
@@ -3,6 +3,113 @@ import matplotlib.pyplot as plt
 import xarray as xr
 from scipy import interpolate, signal
 
+def pvol(dist,profs,pfill,title_str,pnames,imethod='extend',datum=0.4,iverbose=True,iplot=True,iprint=True):
+    """
+    """
+    # Colors from colorbrewer...but one more than needed so we can skip the first one (too light)
+    cols=['#feedde','#fdbe85','#fd8d3c','#e6550d','#a63603']
+    dx = dist[1]-dist[0]
+    nmaps, lp = np.shape(profs)
+    if(iverbose):
+        print('dx: ',dx)
+        print("nmaps, length profiles: ",nmaps,lp)
+
+    if(iverbose and iplot):
+        fig = plt.figure(figsize=(5,3))
+        plt.plot(dist,pfill,':r')
+        for i in range(0,nmaps):
+            plt.plot(dist,profs[i,:],'-',c=cols[i+1])
+
+    # find first good value
+    ix = np.zeros((nmaps))
+    for i in range(0,nmaps):
+        ix[i] = np.argwhere(np.isfinite(profs[i,:]))[0]
+    if(iverbose):
+        print('indices to first good value: ',ix)
+
+    # make a copy of the unchanged profiles for plotting
+    profr = profs.copy()
+
+    # replace NaNs after this point with the fill values
+    for i in range((nmaps)):
+        # replace NaNs after first good value with the fill values
+        idx = np.isnan(profs[i,:])
+        profs[i,idx]=pfill[idx]
+        # replace any other NaNs with zero
+        profs[i,np.isnan(profs[i,:])]=0.
+        # restore values before first good value with NaNs
+        profs[i,:int(ix[i])]=np.nan
+
+    if(imethod is 'extend' ):
+        title_str = title_str+'_extended'
+        if iverbose:
+            print('extend')
+        npts = int(5/dx)
+        # fit a straight line to first 5 points
+        for i in range((nmaps)):
+            p = np.polyfit( dist[int(ix[i]+1):int(ix[i]+1+npts)],\
+                profs[i,int(ix[i]+1):int(ix[i]+1+npts)],1)
+            if(p[0]>0.):
+                # if slope is positive, replace NaNs with line
+                profs[i,0:int(ix[i])]=np.polyval(p,dist[0:int(ix[i])])
+            else:
+                # if slope is not positive, replace NaNs with zeros
+                profs[i,0:int(ix[i])]=0.
+        # for i in range((nmaps)):
+        #     print(np.sum(np.isnan(profs[i])))
+    elif(imethod is 'clip'):
+        title_str = title_str+'_clip'
+        if iverbose:
+            print('clipped')
+        imx = int(np.max(ix))
+        profs[:,0:imx]=0.
+        # for i in range((nmaps)):
+        #     print(np.sum(np.isnan(profs[i])))
+
+    # Calculate volumes
+    profd = profs.copy()-datum
+    profd[np.where(profd<=0.)]=0.
+    v = np.sum(profd,1)*dx
+    if iverbose:
+        print("Profile volumes: ", v)
+
+    # Calculate centroids
+    cxcy = np.zeros((nmaps,2))
+    profc = profs.copy()
+    profc[np.where(profc<=datum)]=np.nan
+    for i in range(0,nmaps):
+        cxcy[i,0],cxcy[i,1] = centroid(dist,profc[i,:])
+    if iverbose:
+        print("Centroids: \n",cxcy)
+
+    if iplot:
+        fig=plt.figure(figsize=(9,6))
+        plt.plot(dist,np.ones_like(dist)*datum,'--',c='dimgray',linewidth=2)
+        for i in range(0,4):
+            lab = '{0} {1: .0f} m$^3$/m'.format(pnames[i],v[i])
+            plt.plot(dist,profr[i,:],'-',linewidth=3,c=cols[i+1],label=lab)
+            plt.plot(dist,profs[i,:],':',linewidth=3,c=cols[i+1])
+        for i in range(0,4):
+            plt.plot(cxcy[i,0],cxcy[i,1],'ok',ms=12)
+            plt.plot(cxcy[i,0],cxcy[i,1],'o',c=cols[i])
+        plt.legend()
+        plt.ylim((-1., 6.))
+        plt.xlim((lp*dx,0)) # this plots xaxis backwards
+        plt.ylabel('Elevation (m NAVD88)')
+        plt.xlabel('Across-shore Distance (m)')
+        plt.title(title_str)
+        if iprint:
+            pfn = 'p_'+title_str+'.svg'
+            plt.savefig(pfn)
+
+    return v, cxcy
+
+
+def centroid(x,z):
+    cz = np.nanmean(z)
+    cx = np.nansum(z*x)/np.nansum(z)
+    return(cx,cz)
+
 def box2UTMh(x, y, x0, y0, theta):
     '''
     2D rotation and translation of x, y
